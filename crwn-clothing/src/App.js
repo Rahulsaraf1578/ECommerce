d5649f01c082bc98dba559ffdf5fa6ed77bd6909
Rahul Saraf <rahulsaraf1578@gmail.com>
2026-02-19 19:40:18 +0530

    Adding signin page

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,22 +1,24 @@
-import Home from "./routes/home/home.componenet";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-const Navigation = () => {
-  return (
+import Home from "./routes/home/home.componenet";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/signin.component";
+
+const Shop = () => {
+  return(
     <div>
-      <div>
-        <h1>I am a navigation bar</h1>
-      </div>
-      <Outlet />
+      <h1>I am shop</h1>
     </div>
-  );
-};
+  )
+}
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
       </Route>
     </Routes>
   );
