8219cb5697072c9d7e649d59e0a53c1c4dca3a4b
Rahul Saraf <rahulsaraf1578@gmail.com>
2026-02-24 19:17:15 +0530

    Created Button component

--- crwn-clothing/src/Components/sign-up-form/sign-up-form.componenet.jsx
+++ crwn-clothing/src/Components/sign-up-form/sign-up-form.componenet.jsx
@@ -5,6 +5,7 @@ import {
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import "./sign-up-form.styles.scss";
+import Button from "../button/button.component";
 
 const defaultFormFields = {
   displayName: "",
@@ -87,7 +88,7 @@ const SignUpForm = () => {
           value={confirmPassword}
           required
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
