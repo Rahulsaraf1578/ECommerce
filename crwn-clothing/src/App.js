2df3dcf82cbc27dc4e2fb16ab425e9e3288db315
Rahul Saraf <rahulsaraf1578@gmail.com>
2026-02-27 08:58:15 +0530

    Created context for shop data

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,22 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.componenet";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return(
-    <div>
-      <h1>I am shop</h1>
-    </div>
-  )
-}
+import Shop from "./routes/shop/Shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop/>}/>
-        <Route path="/auth" element={<Authentication/>}/>
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
