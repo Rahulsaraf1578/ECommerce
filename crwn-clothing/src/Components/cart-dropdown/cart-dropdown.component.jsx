624c17c1e0730df2e0dfe0cb3df40b7302954977
Rahul Saraf <rahulsaraf1578@gmail.com>
2026-02-28 14:27:39 +0530

    Completing cart icon

--- crwn-clothing/src/Components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/Components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,20 +6,18 @@ import Button from "../button/button.component";
 // import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/cart-item.component";
+import { CartContext } from "../../context/cart.context";
 
 const CartDropdown = () => {
-  // const { cartItems } = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
 
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {/* {cartItems.length ? (
-          cartItems.map((cartItem) => (
-            <CartItem key={cartItem.id} cartItem={cartItem} />
-          ))
-        ) : (
-          <span className="empty-message">Your cart is empty</span>
-        )} */}
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
