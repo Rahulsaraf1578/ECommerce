05e79f11bdfc1280b397bd262ce7927351f115c9
Rahul Saraf <rahulsaraf1578@gmail.com>
2026-03-01 08:50:17 +0530

    Category preview and category component

--- crwn-clothing/src/routes/shop/Shop.component.jsx
+++ crwn-clothing/src/routes/shop/Shop.component.jsx
@@ -1,17 +1,14 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../context/product.context";
-import ProductCard from "../../Components/product-card/product-card.component";
-
-import './shop.styles.scss';
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+import "./shop.styles.scss";
+import {Route, Routes} from "react-router-dom";
 
 const Shop = () => {
-  const {products} = useContext(ProductsContext);
   return (
-    <div className="products-container">
-        {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
-}
+};
 export default Shop;
